51a2d253cfef819ff00d9c992bb0b7f468677e37
Julie Pan <ycp217@nyu.edu>
2016-07-18 17:00:59 -0400

    add header template

--- js/components/pages/HomePage.react.js
+++ js/components/pages/HomePage.react.js
@@ -5,17 +5,23 @@ import ReactDataGrid from 'react-data-grid';
 import { AgGridReact } from 'ag-grid-react';
 
 import 'ag-grid-root/dist/styles/ag-grid.css';
-import 'ag-grid-root/dist/styles/theme-bootstrap.css';
+import 'ag-grid-root/dist/styles/theme-fresh.css';
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
     this.onGridReady = this.onGridReady.bind(this);
-    this.addColumn = this.addColumn.bind(this);
+    this._addColumn = this._addColumn.bind(this);
+    this._renderHeaderCell = this._renderHeaderCell.bind(this);
     this.state = {
       columnDefs: [
-        {headerName: 'Make', field: 'make', editable: true},
-        {headerName: 'Model', field: 'model'},
+        {
+          headerName: 'Make',
+          field: 'make',
+          editable: true,
+          headerCellTemplate: this._renderHeaderCell
+        },
+        {headerName: 'Model', field: 'model', editable: true},
         {headerName: 'Price', field: 'price'}
       ],
       rowData: [
@@ -34,6 +40,48 @@ class HomePage extends Component {
     };
   }
 
+  _renderHeaderCell(params) {
+    console.log(params);
+    const header = document.createElement('div');
+
+    //
+    const agResizeBar = document.createElement('div');
+    agResizeBar.classname = 'ag-header-cell-resize';
+
+    //
+    const agMenu = document.createElement('span');
+    agMenu.classname = 'ag-header-icon ag-header-cell-menu-button';
+
+    //
+    const agHeaderCellLabel = document.createElement('div');
+    agHeaderCellLabel.classname = 'ag-header-cell-label';
+    agHeaderCellLabel.id = 'agHeaderCellLabel';
+
+    const agSortAsc = document.createElement('span');
+    agSortAsc.id = 'agSortAsc';
+    const agSortDesc = document.createElement('span');
+    agSortDesc.id = 'agSortDesc';
+    const agNoSort = document.createElement('span');
+    agNoSort.id = 'agNoSort';
+    const agFilter = document.createElement('span');
+    agFilter.id = 'agFilter';
+    const agText = document.createElement('span');
+    agText.id = 'agText';
+
+    agHeaderCellLabel.appendChild(agSortAsc);
+    agHeaderCellLabel.appendChild(agSortDesc);
+    agHeaderCellLabel.appendChild(agNoSort);
+    agHeaderCellLabel.appendChild(agFilter);
+    agHeaderCellLabel.appendChild(agText);
+
+    //
+    header.appendChild(agResizeBar);
+    header.appendChild(agMenu);
+    header.appendChild(agHeaderCellLabel);
+    
+    return header
+  }
+
   componentDidMount() {
   }
 
@@ -45,7 +93,7 @@ class HomePage extends Component {
     this.columnApi = params.columnApi;
   }
 
-  addColumn() {
+  _addColumn() {
     let columns = this.state.columnDefs.concat({ headerName: 'Wha', field: 'whaaaaa'});
     this.setState({ columnDefs: columns });
   }
@@ -53,8 +101,9 @@ class HomePage extends Component {
   render() {
     return (
       <div>
-        <button onClick={this.addColumn}>Add Column</button>
-        <div className='ag-bootstrap'>
+        <button onClick={this._addColumn}>Add Column</button>
+        <button onClick={(e) => this.api.deselectAll()}>Deselect All</button>
+        <div className='ag-fresh'>
           <AgGridReact
             // listening for events
             onGridReady={this.onGridReady.bind(this)}
@@ -69,7 +118,7 @@ class HomePage extends Component {
             enableSorting='true'
             enableFilter='true'
             groupHeaders='true'
-            rowHeight='43'
+            rowHeight='25'
             debug='true'
           />
         </div>
---
+++ js/components/pieces/HeaderCell.react.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+export default class HeaderCell extends Component {
+
+}
