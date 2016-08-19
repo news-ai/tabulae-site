ab7e5b0d3ef7c14273221163584790163f788778
Julie Pan <ycp217@nyu.edu>
2016-08-19 14:59:18 -0400

    remove obsolete HomePage component

--- js/app.js
+++ js/app.js
@@ -31,7 +31,6 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import configureStore from './configureStore';
 
 // Import the pages
-import HomePage from './components/pages/HomePage.react';
 import NotFound from './components/pages/NotFound.react';
 import App from './components/App.react';
 import ListManager from './components/pages/ListManager.react';
@@ -63,7 +62,7 @@ ReactDOM.render(
             <Route path='lists' name='List Manager' component={ListManager}>
           </Route>
           <Route path='lists/new' name='New Sheet' component={NewTable} />
-          <Route path='lists/:listId' staticName={true} name='Sheet' component={Table} />
+          <Route path='lists/:listId' staticName name='Sheet' component={Table} />
           <Route path='archive' name='Archive' component={Archive} />
           <Route path='email' name='Email' component={EmailPanel} />
           <Route path='*' component={NotFound} />
