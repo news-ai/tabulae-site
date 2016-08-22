0dba0fb9f9de54ada93f897cdb1273a6d331a39e
Julie Pan <ycp217@nyu.edu>
2016-08-21 20:53:02 -0400

    add template string highlighting to Subject line

--- js/components/Email/Subject.react.js
+++ js/components/Email/Subject.react.js
@@ -1,13 +1,38 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import {Editor, EditorState} from 'draft-js';
+import CurlySpan from './CurlySpan.react';
+import {
+  Editor,
+  EditorState,
+  CompositeDecorator
+} from 'draft-js';
+
+const CURLY_REGEX = /{([^}]+)}/g;
+
+function findWithRegex(regex, contentBlock, callback) {
+  const text = contentBlock.getText();
+  let matchArr, start;
+  while ((matchArr = regex.exec(text)) !== null) {
+    start = matchArr.index;
+    callback(start, start + matchArr[0].length);
+  }
+}
+
+function curlyStrategy(contentBlock, callback) {
+  findWithRegex(CURLY_REGEX, contentBlock, callback);
+}
 
 class Subject extends Component {
   constructor(props) {
     super(props);
+    const compositeDecorator = new CompositeDecorator([{
+      strategy: curlyStrategy,
+      component: CurlySpan
+    }]);
+
     this.state = {
-      editorState: EditorState.createEmpty()
+      editorState: EditorState.createEmpty(compositeDecorator)
     };
+
     this.onChange = (editorState) => {
       const { _setSubjectLine } = this.props;
       // save subject line to send 
