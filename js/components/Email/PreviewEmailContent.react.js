d9d0a961b4e09ee28341fb0ecd495adcc49c7565
Julie Pan <ycp217@nyu.edu>
2016-08-26 19:05:25 -0400

    show analytics w/o stylings

--- js/components/Email/PreviewEmailContent.react.js
+++ js/components/Email/PreviewEmailContent.react.js
@@ -1,6 +1,9 @@
 import React, { PropTypes } from 'react';
 
 const styles = {
+  analytics: {
+
+  },
   contentBox: {
     border: '1px dotted lightgray',
     margin: '10px',
@@ -16,6 +19,9 @@ const styles = {
   },
   strong: {
     color: 'gray', marginRight: '15px'
+  },
+  bounced: {
+    color: 'red'
   }
 };
 
@@ -23,16 +29,36 @@ function createMarkUp(html) {
   return { __html: html };
 }
 
-function PreviewEmailContent({id, to, subject, body, onSendEmailClick, issent}) {
+function AnalyticsPanel({opened, clicked}) {
+  return (<div className='email-analytics' style={styles.analytics}>
+    <p>Opened: {opened}</p>
+    <p>Clicked: {clicked}</p>
+  </div>);
+}
+
+function PreviewEmailContent(props) {
+  const {
+      id,
+      to,
+      subject,
+      body,
+      onSendEmailClick,
+      issent,
+      bounced,
+      bouncedreason,
+      clicked,
+      opened
+    } = props;
   return (
     <div style={styles.contentBox}>
       <div className='u-full-width' style={styles.content}>
+        {bounced ? <p>Email bounced because of: <span style={styles.bounced}>{bouncedreason}</span></p> : null}
+        {issent ? <AnalyticsPanel {...props} /> : null}
         <p style={styles.span}><strong style={styles.strong}>To</strong>{to}</p>
         <p style={styles.span}><strong style={styles.strong}>Subject</strong>{subject}</p>
         <div style={styles.span} dangerouslySetInnerHTML={createMarkUp(body)} />
       </div>
       {issent ? null : <button className='button' onClick={onSendEmailClick}>Send</button>}
-
     </div>
     );
 }
