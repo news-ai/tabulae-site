b639cf14399f560646a4a79c3d792478fcf22659
Julie Pan <ycp217@nyu.edu>
2016-09-13 13:35:47 -0400

    clean up and fix table bugs

--- js/constants/CustomRenderers.js
+++ js/constants/CustomRenderers.js
@@ -3,13 +3,13 @@ import {yellow100} from 'material-ui/styles/colors';
 
 export function toCenterRenderer(instance, td, row, col, prop, value, cellProperties) {
   Handsontable.renderers.CheckboxRenderer.apply(this, arguments);
-  td.style.textAlign = 'center';
-  return td;
+  td.style.padding = '15px 20px';
+  td.style.boxSizing = 'border-box';
 }
 
 export function outdatedRenderer(instance, td, row, col, prop, value, cellProperties) {
   // different default renderer for each row that is not text-only
-  if (prop === 'selected') toCenterRenderer.apply(this, arguments);
+  if (prop === 'selected') Handsontable.renderers.CheckboxRenderer.apply(this, arguments);
   else Handsontable.renderers.TextRenderer.apply(this, arguments);
 
   td.style.backgroundColor = yellow100;
