26ecffe766e5550b43511d85998c5e2db563bdb5
Julie Pan <ycp217@nyu.edu>
2016-09-28 14:23:02 -0400

    delete is reserved in js. use cap instead

--- js/actions/api.js
+++ js/actions/api.js
@@ -6,7 +6,7 @@ export function get(endpoint) {
     .then( text => JSON.parse(text));
 }
 
-export function delete(endpoint) {
+export function Delete(endpoint) {
   return fetch(`${window.TABULAE_API_BASE}${endpoint}`, {
     method: 'DELETE',
     credentials: 'include'
