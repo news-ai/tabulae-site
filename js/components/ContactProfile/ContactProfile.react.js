4abe410f04304113b277e2f8faf547f39ec8f313
Julie Pan <ycp217@nyu.edu>
2016-09-28 22:04:56 -0400

    add some icon

--- js/components/ContactProfile/ContactProfile.react.js
+++ js/components/ContactProfile/ContactProfile.react.js
@@ -37,16 +37,17 @@ const styles = {
   },
 };
 
-const ContactDescriptor = ({content, contentTitle, onClick, className}) => {
+const ContactDescriptor = ({content, contentTitle, onClick, className, iconClassName}) => {
   return (
     <div className={className} style={{display: 'flex', alignItems: 'center'}}>
+      <i style={{marginRight: 8}} className={iconClassName} aria-hidden='hidden' />
       <span>{content ? content : `${contentTitle} not filled`}</span>
       <IconButton
         style={{marginLeft: 3}}
         iconStyle={styles.smallIcon}
         style={styles.small}
         iconClassName={content ? 'fa fa-edit' : 'fa fa-plus'}
-        tooltip={`Add ${contentTitle}`}
+        tooltip={`${content ? 'Edit' : 'Add'} ${contentTitle}`}
         tooltipPosition='top-right'
         onClick={_ => alertify.prompt(`Enter ${contentTitle}`, '', onClick, function() {})}
         />
@@ -57,12 +58,13 @@ const ContactDescriptor = ({content, contentTitle, onClick, className}) => {
 const ContactProfileDescriptions = ({contact, patchContact, className}) => {
   return (
     <div className={className} style={{marginTop: 5}}>
-      <h4>{contact.firstname} {contact.lastname}</h4>
-      <ContactDescriptor className='large-12 medium-8 small-12 columns' content={contact.email} contentTitle='Email' onClick={(e, value) => isEmail(value) && patchContact(contact.id, {email: value})}/>
-      <ContactDescriptor className='large-12 medium-8 small-12 columns' content={contact.blog} contentTitle='Blog' onClick={(e, value) => isURL(value) && patchContact(contact.id, {blog: value})}/>
-      <ContactDescriptor className='large-12 medium-8 small-12 columns' content={contact.twitter} contentTitle='Twitter' onClick={(e, value) => patchContact(contact.id, {twitter: value})}/>
-      <ContactDescriptor className='large-12 medium-8 small-12 columns' content={contact.linkedin} contentTitle='LinkedIn' onClick={(e, value) => isURL(value) && patchContact(contact.id, {linkedin: value})}/>
-      <ContactDescriptor className='large-12 medium-8 small-12 columns' content={contact.website} contentTitle='Website' onClick={(e, value) => isURL(value) && patchContact(contact.id, {website: value})}/>
+      <h4 style={{marginLeft: 10}}>{contact.firstname} {contact.lastname}</h4>
+      <ContactDescriptor iconClassName='fa fa-envelope' className='large-12 medium-8 small-12 columns' content={contact.email} contentTitle='Email' onClick={(e, value) => isEmail(value) && patchContact(contact.id, {email: value})}/>
+      <ContactDescriptor iconClassName='fa fa-rss' className='large-12 medium-8 small-12 columns' content={contact.blog} contentTitle='Blog' onClick={(e, value) => isURL(value) && patchContact(contact.id, {blog: value})}/>
+      <ContactDescriptor iconClassName='fa fa-twitter' className='large-12 medium-8 small-12 columns' content={contact.twitter} contentTitle='Twitter' onClick={(e, value) => patchContact(contact.id, {twitter: value})}/>
+      <ContactDescriptor iconClassName='fa fa-instagram' className='large-12 medium-8 small-12 columns' content={contact.instagram} contentTitle='Instagram' onClick={(e, value) => patchContact(contact.id, {instagram: value})}/>
+      <ContactDescriptor iconClassName='fa fa-linkedin' className='large-12 medium-8 small-12 columns' content={contact.linkedin} contentTitle='LinkedIn' onClick={(e, value) => isURL(value) && patchContact(contact.id, {linkedin: value})}/>
+      <ContactDescriptor iconClassName='fa fa-external-link' className='large-12 medium-8 small-12 columns' content={contact.website} contentTitle='Website' onClick={(e, value) => isURL(value) && patchContact(contact.id, {website: value})}/>
     </div>);
 };
 
