276d851c93ad39256b9e4e4a5af92cbc3daf375b
Julie Pan <ycp217@nyu.edu>
2016-09-28 22:46:51 -0400

    revamp settings for feeds

--- js/components/ContactProfile/FeedsController.react.js
+++ js/components/ContactProfile/FeedsController.react.js
@@ -6,7 +6,6 @@ import * as feedActions from './actions';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
-import IconButton from 'material-ui/IconButton';
 import Dialog from 'material-ui/Dialog';
 import Chip from 'material-ui/Chip';
 import {grey500} from 'material-ui/styles/colors';
@@ -17,10 +16,8 @@ class FeedsController extends Component {
     this.state = {
       feedUrl: '',
       isPanelOpen: false,
-      isDeletePanelOpen: false
     };
     this.togglePanel = _ => this.setState({isPanelOpen: !this.state.isPanelOpen});
-    this.toggleDeletePanel = _ => this.setState({isDeletePanelOpen: !this.state.isDeletePanelOpen});
     this.addFeedClick = this._addFeedClick.bind(this);
   }
 
@@ -29,40 +26,38 @@ class FeedsController extends Component {
     if (this.state.feedUrl.length === 0) return;
     props.addFeed(props.contactId, props.listId, this.state.feedUrl);
     this.togglePanel();
+    window.location.reload();
   }
 
   render() {
     const props = this.props;
     const state = this.state;
-    const addFeedActions = [
-      <FlatButton
-        label='Cancel'
-        primary
-        onTouchTap={this.togglePanel}
-      />,
-      <FlatButton
-        label='Submit'
-        primary
-        keyboardFocused
-        onTouchTap={this.addFeedClick}
-      />,
-    ];
+
     return (
       <div>
-        <Dialog actions={addFeedActions} title='New Author RSS Feed' modal open={state.isPanelOpen} onRequestClose={this.togglePanel}>
-          <TextField
-          value={state.feedUrl}
-          hintText='Enter RSS Url here'
-          errorText={validator.isURL(state.feedUrl) || state.feedUrl.length === 0 ? null : 'not valid URL'}
-          onChange={e => this.setState({feedUrl: e.target.value})}
-          />
-        </Dialog>
-        <Dialog open={state.isDeletePanelOpen} onRequestClose={this.toggleDeletePanel}>
-          <span>Hit 'Refresh' after deletes are made to see changes</span>
-          {props.feeds && props.feeds.map((feed, i) => <Chip
-            style={{margin: 4}}
-            key={i}
-            onRequestDelete={_ => props.deleteFeed(feed.id)}>{feed.url}</Chip>)}
+        <Dialog autoScrollBodyContent open={state.isPanelOpen} onRequestClose={this.togglePanel}>
+          <h5>Settings</h5>
+          <div style={{margin: 20}}>
+            <span style={{marginRight: 10}}>Attach New RSS Feed</span>
+            <TextField
+            value={state.feedUrl}
+            hintText='Enter RSS link here'
+            errorText={validator.isURL(state.feedUrl) || state.feedUrl.length === 0 ? null : 'not valid URL'}
+            onChange={e => this.setState({feedUrl: e.target.value})}
+            />
+            <FlatButton
+            label='Add'
+            primary
+            onTouchTap={this.addFeedClick}
+            />
+          </div>
+          <div style={{margin: 20}}>
+            <span>Currently Attached Feeds:</span>
+            {props.feeds && props.feeds.map((feed, i) => <Chip
+              style={{margin: 4}}
+              key={i}
+              onRequestDelete={_ => props.deleteFeed(feed.id).then(_ => props.fetchContactFeeds(props.contactId))}>{feed.url}</Chip>)}
+          </div>
         </Dialog>
         <div className='row' style={{marginTop: 15, marginBottom: 15}}>
           <div className='large-9 medium-8 small-12 columns' style={{color: grey500, fontSize: '0.7em'}}>
@@ -70,8 +65,7 @@ class FeedsController extends Component {
             {props.attachedfeeds && props.attachedfeeds.map((feed, i) => <div key={i} style={{marginLeft: 3}}><span>{feed}</span></div>)}
           </div>
           <div className='large-3 medium-4 small-12 columns vertical-center'>
-            <RaisedButton style={{marginTop: 10, marginBottom: 10}} label='Add New Feed' onClick={this.togglePanel} labelStyle={{textTransform: 'none'}} />
-            <IconButton iconClassName='fa fa-times' style={{marginTop: 10, marginBottom: 10, float: 'right'}} tooltip='Delete Feed' onClick={this.toggleDeletePanel} />
+            <RaisedButton style={{marginTop: 10, marginBottom: 10, float: 'right'}} label='Settings' onClick={this.togglePanel} labelStyle={{textTransform: 'none'}} />
           </div>
         </div>
       </div>
@@ -94,7 +88,8 @@ const mapStateToProps = (state, props) => {
 const mapDispatchToProps = dispatch => {
   return {
     addFeed: (contactid, listid, url) => dispatch(feedActions.addFeed(contactid, listid, url)),
-    deleteFeed: feedId => dispatch(feedActions.deleteFeed(feedId))
+    deleteFeed: feedId => dispatch(feedActions.deleteFeed(feedId)),
+    fetchContactFeeds: (contactId) => dispatch(feedActions.fetchContactFeeds(contactId)),
   };
 };
 
