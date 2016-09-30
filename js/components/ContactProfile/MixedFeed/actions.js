0ea5f5743fa5e66bfa3d6298606b2aaf57092927
Julie Pan <ycp217@nyu.edu>
2016-09-30 19:05:48 -0400

    add pagination to mixed feed

--- js/components/ContactProfile/MixedFeed/actions.js
+++ js/components/ContactProfile/MixedFeed/actions.js
@@ -5,21 +5,22 @@ const feedSchema = new Schema('feeds');
 const tweetSchema = new Schema('tweets');
 
 export function fetchMixedFeed(contactId) {
-  return dispatch => {
+  const PAGE_LIMIT = 50;
+  return (dispatch, getState) => {
+    const contactObj = getState().mixedReducer[contactId];
+    const OFFSET = contactObj ? contactObj.offset : 0;
+    const isReceiving = getState().mixedReducer.isReceiving;
+    if (OFFSET === null || isReceiving) return;
     dispatch({type: mixedConstant.REQUEST_MULTIPLE, contactId});
-    return api.get(`/contacts/${contactId}/feed`)
+    return api.get(`/contacts/${contactId}/feed?limit=${PAGE_LIMIT}&offset=${OFFSET}`)
     .then(response => {
       console.log(response);
-      // const res = normalize(response, {
-      //   data: arrayOf(feedSchema),
-      // });
-      // return dispatch({
-      //   type: feedConstant.RECEIVE_MULTIPLE,
-      //   feeds: res.entities.feeds,
-      //   ids: res.result.data,
-      //   contactId
-      // });
-      return dispatch({type: mixedConstant.RECEIVE_MULTIPLE, feed: response.data, contactId, offset: 0});
+      const newOffset = response.data.length < PAGE_LIMIT ? null : OFFSET + PAGE_LIMIT;
+      return dispatch({
+        type: mixedConstant.RECEIVE_MULTIPLE,
+        feed: response.data,
+        contactId,
+        offset: newOffset});
     })
     .catch(err => console.log(err));
   };
