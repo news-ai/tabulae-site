1c0b6d89ee9d445a45a429c85eb2d8c66fa19589
Julie Pan <ycp217@nyu.edu>
2016-10-02 00:31:45 -0400

    add date

--- js/components/ContactProfile/ContactProfile.react.js
+++ js/components/ContactProfile/ContactProfile.react.js
@@ -89,10 +89,14 @@ const tweetStyle = {
   borderRadius: '0.4em'
 };
 
-const Tweet = ({text, username}) => {
+const Tweet = ({text, username, createdat}) => {
+  const date = new Date(createdat);
   return <div className='row' style={tweetStyle}>
     <div className='large-10 medium-9 small-8 columns'><span>{text}</span></div>
     <div className='large-2 medium-3 small-4 columns'><span style={{float: 'right'}}>{username}</span></div>
+     <div className='large-12 medium-12 small-12 columns' style={{fontSize: '0.8em'}}>
+      <span>{date.toDateString()}</span><span style={{marginLeft: 8}}>{date.toTimeString()}</span>
+    </div>
   </div>;
 };
 
