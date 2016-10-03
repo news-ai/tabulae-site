8ed59b5936ce0c6575f22589447fe2ecc978247a
Julie Pan <ycp217@nyu.edu>
2016-10-03 14:39:42 -0400

    Add link to tweet. Close #240

--- js/components/ContactProfile/Tweets/Tweet.react.js
+++ js/components/ContactProfile/Tweets/Tweet.react.js
@@ -8,11 +8,15 @@ const tweetStyle = {
   border: `dotted 1px ${grey400}`,
   borderRadius: '0.4em'
 };
-const Tweet = ({text, username, createdat}) => {
+const Tweet = ({text, username, createdat, tweetidstr}) => {
   const date = new Date(createdat);
   return <div className='row' style={tweetStyle}>
     <div className='large-10 medium-9 small-8 columns'><span>{text}</span></div>
-    <div className='large-2 medium-3 small-4 columns'><span style={{float: 'right'}}>{username}</span></div>
+    <div className='large-2 medium-3 small-4 columns'>
+      <span style={{float: 'right'}}>
+        {tweetidstr ? <a target='_blank' href={`https://twitter.com/statuses/${tweetidstr}`}>{username}</a> : username}
+      </span>
+    </div>
      <div className='large-12 medium-12 small-12 columns' style={{fontSize: '0.8em'}}>
       <span>{date.toDateString()}</span><span style={{marginLeft: 8}}>{date.toTimeString()}</span>
     </div>
