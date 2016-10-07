6f025f188e78812f77b80305be7b1410e2d6680d
Julie Pan <ycp217@nyu.edu>
2016-10-06 21:59:39 -0400

    separate lists and archived lists data fetching

--- js/actions/listActions.js
+++ js/actions/listActions.js
@@ -86,14 +86,37 @@ export function fetchLists() {
     if (OFFSET === null || getState().listReducer.isReceiving) return;
     dispatch(requestLists());
     return api.get(`/lists?limit=${PAGE_LIMIT}&offset=${OFFSET}&order=-Created`)
-    .then( response => {
+    .then(response => {
       const res = normalize(response, {
         data: arrayOf(listSchema),
       });
       const newOffset = response.data.length < PAGE_LIMIT ? null : OFFSET + PAGE_LIMIT;
       dispatch(receiveLists(res.entities.lists, res.result.data, newOffset));
     })
-    .catch( message => dispatch(requestListsFail(message)));
+    .catch(message => dispatch(requestListsFail(message)));
+  };
+}
+
+export function fetchArchivedLists() {
+  const PAGE_LIMIT = 50;
+  return (dispatch, getState) => {
+    const OFFSET = getState().listReducer.archivedOffset;
+    if (OFFSET === null || getState().listReducer.isReceiving) return;
+    dispatch(requestLists());
+    return api.get(`/lists/archived?limit=${PAGE_LIMIT}&offset=${OFFSET}&order=-Created`)
+    .then(response => {
+      const res = normalize(response, {
+        data: arrayOf(listSchema),
+      });
+      const newOffset = response.data.length < PAGE_LIMIT ? null : OFFSET + PAGE_LIMIT;
+      dispatch({
+        type: listConstant.RECEIVE_MULTIPLE,
+        lists: res.entities.lists,
+        ids: res.result.data,
+        archivedOffset: newOffset
+      });
+    })
+    .catch(message => dispatch(requestListsFail(message)));
   };
 }
 
@@ -125,7 +148,6 @@ export function createEmptyList(name) {
     .catch(message => console.log(message));
   };
 }
-
 
 export function archiveListToggle(listId) {
   return (dispatch, getState) => {
