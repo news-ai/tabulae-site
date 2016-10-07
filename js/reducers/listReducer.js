cace9c0ef70ad367ca911b678a31c7222bede61a
Julie Pan <ycp217@nyu.edu>
2016-10-06 22:07:56 -0400

    NaN bug with setting offsets

--- js/reducers/listReducer.js
+++ js/reducers/listReducer.js
@@ -57,8 +57,8 @@ function listReducer(state = initialState.listReducer, action) {
       obj.lists = unarchivedLists;
       obj.archivedLists = archivedLists;
       obj.isReceiving = false;
-      if (!action.archivedOffset) obj.offset = action.offset;
-      if (!action.offset) obj.archivedOffset = action.archivedOffset;
+      if (action.archivedOffset === undefined) obj.offset = action.offset;
+      if (action.offset === undefined) obj.archivedOffset = action.archivedOffset;
       return obj;
     case listConstant.REQUEST_FAIL:
       obj.isReceiving = false;
