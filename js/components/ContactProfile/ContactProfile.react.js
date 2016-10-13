fdd2b3f27a2c84b4cc31e3e35ec6a7b4ccedc749
Julie Pan <ycp217@nyu.edu>
2016-10-12 22:52:08 -0400

    add flag if handle invalid

--- js/components/ContactProfile/ContactProfile.react.js
+++ js/components/ContactProfile/ContactProfile.react.js
@@ -37,13 +37,21 @@ const styles = {
 };
 
 const ContactProfileDescriptions = ({contact, patchContact, className, list}) => {
+  let instagramErrorText = null;
+  if (contact.instagraminvalid) instagramErrorText = 'Invalid Instagram handle';
+  else if (contact.instagramprivate) instagramErrorText = 'Instagram is private';
+
+  let twitterErrorText = null;
+  if (contact.twitterinvalid) twitterErrorText = 'Invalid Twitter handle';
+  else if (contact.twitterprivate) twitterErrorText = 'Twitter is private';
+
   return (
     <div className={className} style={{marginTop: 5}}>
       <h4 style={{marginLeft: 10}}>{contact.firstname} {contact.lastname}</h4>
       <ContactDescriptor iconClassName='fa fa-envelope' className='large-12 medium-8 small-12 columns' content={contact.email} contentTitle='Email' onClick={(e, value) => isEmail(value) && patchContact(contact.id, {email: value})}/>
       <ContactDescriptor iconClassName='fa fa-rss' className='large-12 medium-8 small-12 columns' content={contact.blog} contentTitle='Blog' onClick={(e, value) => isURL(value) && patchContact(contact.id, {blog: value})}/>
-      <ContactDescriptor iconClassName='fa fa-twitter' className='large-12 medium-8 small-12 columns' content={contact.twitter} contentTitle='Twitter' onClick={(e, value) => patchContact(contact.id, {twitter: value})}/>
-      <ContactDescriptor iconClassName='fa fa-instagram' className='large-12 medium-8 small-12 columns' content={contact.instagram} contentTitle='Instagram' onClick={(e, value) => patchContact(contact.id, {instagram: value})}/>
+      <ContactDescriptor iconClassName='fa fa-twitter' className='large-12 medium-8 small-12 columns' errorText={twitterErrorText} content={contact.twitter} contentTitle='Twitter' onClick={(e, value) => patchContact(contact.id, {twitter: value})}/>
+      <ContactDescriptor iconClassName='fa fa-instagram' className='large-12 medium-8 small-12 columns' errorText={instagramErrorText} content={contact.instagram} contentTitle='Instagram' onClick={(e, value) => patchContact(contact.id, {instagram: value})}/>
       <ContactDescriptor iconClassName='fa fa-linkedin' className='large-12 medium-8 small-12 columns' content={contact.linkedin} contentTitle='LinkedIn' onClick={(e, value) => isURL(value) && patchContact(contact.id, {linkedin: value})}/>
       <ContactDescriptor iconClassName='fa fa-external-link' className='large-12 medium-8 small-12 columns' content={contact.website} contentTitle='Website' onClick={(e, value) => isURL(value) && patchContact(contact.id, {website: value})}/>
       <div style={{marginTop: 10, marginBottom: 30}}>
