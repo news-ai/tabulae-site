1f785668362d70583cdd3c977c425a3119ab43ed
Julie Pan <ycp217@nyu.edu>
2016-10-14 00:41:37 -0400

    virtualize ListFeed

--- js/components/ListFeed/ListFeed.react.js
+++ js/components/ListFeed/ListFeed.react.js
@@ -5,35 +5,64 @@ import HeadlineItem from '../ContactProfile/Headlines/HeadlineItem.react';
 import Tweet from '../ContactProfile/Tweets/Tweet.react';
 import InstagramItem from '../ContactProfile/Instagram/InstagramItem.react';
 import InfiniteScroll from '../InfiniteScroll';
+import {List, CellMeasurer} from 'react-virtualized';
 
 class ListFeed extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
+    this.rowRenderer = this._rowRenderer.bind(this);
   }
 
   componentWillMount() {
     this.props.fetchListFeed(this.props.listId);
   }
 
+  _rowRenderer({key, index, style}) {
+    const feedItem = this.props.listfeed[index];
+    let row;
+    switch (feedItem.type) {
+      case 'headlines':
+        row = <HeadlineItem {...feedItem} />;
+        break;
+      case 'tweets':
+        row = <Tweet {...feedItem} />;
+        break;
+      case 'instagrams':
+        row = <InstagramItem {...feedItem} />;
+        break;
+      default:
+        row = <HeadlineItem {...feedItem} />;
+    }
+    return (
+      <div key={key} style={style}>
+      {row}
+      </div>);
+  }
+
   render() {
     const props = this.props;
     return (
-      <InfiniteScroll onScrollBottom={_ => props.fetchListFeed(props.listId)}>
-      {props.listfeed && props.listfeed.map((obj, i) => {
-        switch (obj.type) {
-          case 'headlines':
-            return <HeadlineItem key={i} {...obj} />;
-          case 'tweets':
-            return <Tweet key={i} {...obj} />;
-          case 'instagrams':
-            return <InstagramItem key={i} {...obj} />;
-          default:
-            return <HeadlineItem key={i} {...obj} />;
-          }
-        })}
-      </InfiniteScroll>);
+      <div>
+      {props.listfeed &&
+        <CellMeasurer
+        cellRenderer={({rowIndex, ...rest}) => this.rowRenderer({index: rowIndex, ...rest})}
+        columnCount={1}
+        >
+        {({getRowHeight}) => (
+          <List
+          width={1200}
+          height={600}
+          rowCount={props.listfeed.length}
+          rowRenderer={this.rowRenderer}
+          rowHeight={getRowHeight}
+          onScroll={({scrollHeight, scrollTop, clientHeight}) => {
+            if (((scrollHeight - scrollTop) / clientHeight) < 2) props.fetchListFeed(props.listId);
+          }}
+          />
+          )}
+        </CellMeasurer>}
+      </div>);
   }
 }
 
