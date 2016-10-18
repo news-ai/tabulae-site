2a3f83eacbf682c6efe82657d9a0776e0ca32973
Julie Pan <ycp217@nyu.edu>
2016-10-17 23:27:47 -0400

    basic template up for adding contact

---
+++ js/components/ListTable/AddContact.react.js
@@ -0,0 +1,103 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import Dialog from 'material-ui/Dialog';
+import FontIcon from 'material-ui/FontIcon';
+import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
+
+const textfieldStyle = {
+  marginLeft: 10
+};
+
+class AddContact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+    this.onSubmit = this._onSubmit.bind(this);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.open === false && this.state.open === true) {
+      // onRequestOpen hit
+    }
+  }
+
+  _onSubmit() {
+    console.log(this.refs.firstname.input.value);
+  }
+
+  render() {
+    const props = this.props;
+    const state = this.state;
+    const actions = [
+      <FlatButton
+        label='Cancel'
+        primary
+        onTouchTap={_ => this.setState({open: false})}
+      />,
+      <FlatButton
+        label='Submit'
+        primary
+        keyboardFocused
+        onTouchTap={this.onSubmit}
+      />,
+    ];
+    return (
+      <div>
+        <Dialog autoScrollBodyContent modal actions={actions} open={state.open} title='Add Contact' onRequestClose={_ => this.setState({open: false})}>
+          {props.isReceiving && <FontIcon className={'fa fa-spinner fa-spin'} />}
+          <div className='row' style={{marginTop: 20}}>
+            <div className='large-6 medium-12 small-12 columns vertical-center'>
+              <span>Firstname</span><TextField style={textfieldStyle} ref='firstname' name='firstname' />
+            </div>
+            <div className='large-6 medium-12 small-12 columns vertical-center'>
+              <span>Lastname</span><TextField style={textfieldStyle} ref='lastname' name='lastname' />
+            </div>
+            <div className='large-6 medium-12 small-12 columns vertical-center'>
+              <span>Email</span><TextField style={textfieldStyle} ref='email' name='email' />
+            </div>
+            <div className='large-6 medium-12 small-12 columns vertical-center'>
+              <span>Twitter</span><TextField style={textfieldStyle} ref='twitter' name='twitter' />
+            </div>
+            <div className='large-6 medium-12 small-12 columns vertical-center'>
+              <span>Instagram</span><TextField style={textfieldStyle} ref='instagram' name='instagram' />
+            </div>
+            <div className='large-6 medium-12 small-12 columns vertical-center'>
+              <span>LinkedIn</span><TextField style={textfieldStyle} ref='linkedin' name='linkedin' />
+            </div>
+            <div className='large-6 medium-12 small-12 columns vertical-center'>
+              <span>Phone #</span><TextField style={textfieldStyle} ref='phone' name='phone' />
+            </div>
+            <div className='large-6 medium-12 small-12 columns vertical-center'>
+              <span>Blog</span><TextField style={textfieldStyle} ref='blog' name='blog' />
+            </div>
+            <div className='large-6 medium-12 small-12 columns vertical-center'>
+              <span>Website</span><TextField style={textfieldStyle} ref='website' name='website' />
+            </div>
+            <div className='large-6 medium-12 small-12 columns vertical-center'>
+              <span>Notes</span><TextField style={textfieldStyle} ref='notes' name='notes' />
+            </div>
+          </div>
+        </Dialog>
+        {props.children({
+          onRequestOpen: _ => this.setState({open: true})
+        })}
+      </div>);
+  }
+}
+
+const mapStateToProps = (state, props) => {
+  return {
+  };
+};
+
+const mapDispatchToProps = (dispatch, props) => {
+  return {
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddContact);
+
