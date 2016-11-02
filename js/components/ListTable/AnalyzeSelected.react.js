54c059bf023fc94ee38ab0b7318726ca6cdbf9aa
Julie Pan <ycp217@nyu.edu>
2016-11-02 00:16:25 -0400

    add waiting

--- js/components/ListTable/AnalyzeSelected.react.js
+++ js/components/ListTable/AnalyzeSelected.react.js
@@ -4,8 +4,9 @@ import * as actionCreators from '../../actions/AppActions';
 import * as twitterDataActions from '../ContactProfile/SocialDataGraphs/Twitter/actions';
 import withRouter from 'react-router/lib/withRouter';
 
+import Waiting from '../Waiting';
 import Dialog from 'material-ui/Dialog';
-import {XAxis, YAxis, CartesianGrid, Legend, Line, Tooltip, LineChart} from 'recharts';
+import {XAxis, YAxis, CartesianGrid, Line, Tooltip, LineChart} from 'recharts';
 import {red300, blue300, purple300, cyan300, green300, indigo300, orange300} from 'material-ui/styles/colors';
 
 const colors = [red300, blue300, purple300, cyan300, green300, indigo300, orange300];
@@ -34,8 +35,6 @@ class AnalyzeSelected extends Component {
   render() {
     const state = this.state;
     const props = this.props;
-    console.log(props.isReceiving);
-    console.log(props.dataMap);
     return (
       <div>
         <Dialog
@@ -45,27 +44,32 @@ class AnalyzeSelected extends Component {
         autoScrollBodyContent
         onRequestClose={_ => this.setState({open: false})}
         >
-        {props.selected.length > 0 && state.open && props.isReceiving &&
-          props.dataKeys.map((dataKey, i) => (
-            <div>
-              <span>{dataKey}</span>
-              <LineChart
-              key={i}
-              width={550}
-              height={300}
-              data={props.dataMap[dataKey]}
-              margin={{top: 5, right: 40, left: 20, bottom: 5}}>
-                <XAxis dataKey='CreatedAt'/>
-                <YAxis/>
-                <CartesianGrid strokeDasharray='3 3'/>
-                <Tooltip/>
-                {props.contacts.map((id, index) => (
-                  <Line key={index} type='monotone' dataKey={id} stroke={colors[i]} activeDot={{r: 8}}/>
-                  ))}
-              </LineChart>
-            </div>
-              ))
-        }
+          <Waiting isReceiving={props.isReceiving}/>
+          {props.selected.length > 0 && state.open && !props.isReceiving &&
+            props.dataKeys.map((dataKey, i) => (
+              <div className='row' style={{margin: '10px 0'}}>
+                <div className='large-12 medium-12 small-12 columns'>
+                  <h5>{dataKey}</h5>
+                </div>
+                <div className='large-12 medium-12 small-12 columns'>
+                  <LineChart
+                  key={i}
+                  width={550}
+                  height={300}
+                  data={props.dataMap[dataKey]}
+                  margin={{top: 5, right: 40, left: 20, bottom: 5}}>
+                    <XAxis dataKey='dateString'/>
+                    <YAxis/>
+                    <CartesianGrid strokeDasharray='3 3'/>
+                    <Tooltip/>
+                    {props.contacts.map((id, index) => (
+                      <Line key={index} type='monotone' dataKey={id} stroke={colors[i]} activeDot={{r: 8}}/>
+                      ))}
+                  </LineChart>
+                </div>
+              </div>
+                ))
+          }
         </Dialog>
         {props.children({
           onRequestOpen: _ => this.setState({open: true})
@@ -83,7 +87,11 @@ const mapStateToProps = (state, props) => {
     dataKeys.map(dataKey => {
       let data = [];
       for (let i = 0; i < state.twitterDataReducer[filledIds[0]].received.length; i++) {
-        let obj = {CreatedAt: state.twitterDataReducer[filledIds[0]].received[i].CreatedAt};
+        const dateObj = new Date(state.twitterDataReducer[filledIds[0]].received[i].CreatedAt);
+        let obj = {
+          CreatedAt: state.twitterDataReducer[filledIds[0]].received[i].CreatedAt,
+          dateString: `${dateObj.getMonth()}-${dateObj.getDay()}`
+        };
         filledIds.map(contactId => {
           if (state.twitterDataReducer[contactId].received[i]) {
             obj[contactId] = state.twitterDataReducer[contactId].received[i][dataKey];
