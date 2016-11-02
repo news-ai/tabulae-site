669e7d85c6fecfa8be27ce0270468608c2fbba03
Julie Pan <ycp217@nyu.edu>
2016-11-02 00:55:40 -0400

    ey line them up

--- js/components/ListTable/AnalyzeSelected.react.js
+++ js/components/ListTable/AnalyzeSelected.react.js
@@ -48,27 +48,31 @@ class AnalyzeSelected extends Component {
           <Waiting isReceiving={props.isReceiving}/>
           {props.selected.length > 0 && state.open && !props.isReceiving &&
             props.dataKeys.map((dataKey, i) => (
-              <div className='row' style={{margin: '10px 0'}}>
-                <div className='large-12 medium-12 small-12 columns'>
-                  <h5>{dataKey}</h5>
+              <div className='row'>
+                <div className='large-6 medium-12 small-12 columns'>
+                  <div className='row'>
+                    <h5>{dataKey}</h5>
+                  </div>
+                  <div className='row'>
+                    <LineChart
+                    syncId='syncdiz'
+                    key={i}
+                    width={720}
+                    height={250}
+                    data={props.dataMap[dataKey]}
+                    margin={{top: 5, right: 40, left: 20, bottom: 5}}>
+                      <XAxis dataKey='dateString'/>
+                      <YAxis/>
+                      <CartesianGrid strokeDasharray='3 3'/>
+                      <Tooltip/>
+                      {props.twitterHandles.map((handle, index) => (
+                        <Line key={`${dataKey}-${index}`} type='monotone' dataKey={handle} stroke={colors[index]} activeDot={{r: 8}}/>
+                        ))}
+                    </LineChart>
+                  </div>
                 </div>
-                <div className='large-12 medium-12 small-12 columns'>
-                  <LineChart
-                  key={i}
-                  width={550}
-                  height={300}
-                  data={props.dataMap[dataKey]}
-                  margin={{top: 5, right: 40, left: 20, bottom: 5}}>
-                    <XAxis dataKey='dateString'/>
-                    <YAxis/>
-                    <CartesianGrid strokeDasharray='3 3'/>
-                    <Tooltip/>
-                    {props.twitterHandles.map((handle, index) => (
-                      <Line key={`${dataKey}-${index}`} type='monotone' dataKey={handle} stroke={colors[i]} activeDot={{r: 8}}/>
-                      ))}
-                  </LineChart>
-                </div>
-              </div>))}
+              </div>
+                ))}
         </Dialog>
         {props.children({
           onRequestOpen: _ => this.setState({open: true})
