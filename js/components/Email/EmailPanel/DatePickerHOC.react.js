12be5500a3dddb7aa256ce7b2f7c4a0c2af4e8a7
Julie Pan <ycp217@nyu.edu>
2016-11-20 00:29:41 -0500

    get a list of offsets

--- js/components/Email/EmailPanel/DatePickerHOC.react.js
+++ js/components/Email/EmailPanel/DatePickerHOC.react.js
@@ -7,7 +7,7 @@ import MenuItem from 'material-ui/MenuItem';
 import FlatButton from 'material-ui/FlatButton';
 import Toggle from 'material-ui/Toggle';
 
-import moment from 'moment';
+import moment from 'moment-timezone';
 
 import 'node_modules/react-datepicker/dist/react-datepicker.css';
 
@@ -23,6 +23,43 @@ for (let i = 0; i < 60; i++) {
   minutes.push(<MenuItem value={i} key={`minute-${i}`} primaryText={`${i}`} />);
 }
 
+const timezone_names = [
+  {offset: -11.0, text: '(GMT -11:00) Midway Island, Samoa'},
+  {offset: -10.0, text: '(GMT -10:00) Hawaii'},
+  {offset: -9.0, text: '(GMT -9:00) Alaska'},
+  {offset: -8.0, text: '(GMT -8:00) Pacific Time (US & Canada)'},
+  {offset: -12.0, text: '(GMT -12:00) Eniwetok, Kwajalein'},
+  {offset: -7.0, text: '(GMT -7:00) Mountain Time (US & Canada)'},
+  {offset: -6.0, text: '(GMT -6:00) Central Time (US & Canada)'},
+  {offset: -5.0, text: '(GMT -5:00) Eastern Time (US & Canada)'},
+  {offset: -4.0, text: '(GMT -4:00) Atlantic Time (Canada)'},
+  {offset: -3.5, text: '(GMT -3:30) Newfoundland'},
+  {offset: -3.0, text: '(GMT -3:00) Brazil, Buenos Aires, Georgetown'},
+  {offset: -2.0, text: '(GMT -2:00) Mid-Atlantic'},
+  {offset: -1.0, text: '(GMT -1:00) Azores, Cape Verde Islands'},
+  {offset: 0.0, text: '(GMT) Western Europe Time, London'},
+  {offset: 1.0, text: '(GMT +1:00) Brussels, Copenhagen, Madrid, Paris'},
+  {offset: 2.0, text: '(GMT +2:00) Kaliningrad, South Africa'},
+  {offset: 3.0, text: '(GMT +3:00) Baghdad, Riyadh, Moscow, St. Petersburg'},
+  {offset: 3.5, text: '(GMT +3:30) Tehran'},
+  {offset: 4.0, text: '(GMT +4:00) Abu Dhabi, Muscat, Baku, Tbilisi'},
+  {offset: 4.5, text: '(GMT +4:30) Kabul'},
+  {offset: 5.0, text: '(GMT +5:00) Ekaterinburg, Islamabad, Karachi, Tashkent'},
+  {offset: 5.5, text: '(GMT +5:30) Bombay, Calcutta, Madras, New Delhi'},
+  {offset: 5.75, text: '(GMT +5:45) Kathmandu'},
+  {offset: 6.0, text: '(GMT +6:00) Almaty, Dhaka, Colombo'},
+  {offset: 7.0, text: '(GMT +7:00) Bangkok, Hanoi, Jakarta'},
+  {offset: 8.0, text: '(GMT +8:00) Beijing, Perth, Singapore, Hong Kong'},
+  {offset: 9.0, text: '(GMT +9:00) Tokyo, Seoul, Osaka'},
+  {offset: 9.5, text: '(GMT +9:30) Adelaide, Darwin'},
+  {offset: 10.0, text: '(GMT +10:00) Eastern Australia, Guam, Vladivostok'},
+  {offset: 11.0, text: '(GMT +11:00) Magadan, Solomon Islands, New Caledonia'},
+  {offset: 12.0, text: '(GMT +12:00) Auckland, Wellington, Fiji, Kamchatka'}
+];
+
+const timezones = timezone_names
+.map((timezone, i) => <MenuItem value={timezone.text} key={`timezone-${i}`} primaryText={timezone.text}/>);
+
 class DatePickerHOC extends Component {
   constructor(props) {
     super(props);
@@ -33,7 +70,8 @@ class DatePickerHOC extends Component {
       date: rightNow,
       hour: rightNow.hours(),
       minute: rightNow.minutes(),
-      toggled: false
+      toggled: false,
+      timezone: moment.tz.guess(),
     };
     this.onToggle = this._onToggle.bind(this);
   }
@@ -43,6 +81,7 @@ class DatePickerHOC extends Component {
       const today = new Date();
       const rightNow = moment(today);
       this.state = {
+        open: true,
         date: rightNow,
         hour: rightNow.hours(),
         minute: rightNow.minutes(),
@@ -83,6 +122,12 @@ class DatePickerHOC extends Component {
                 </DropDownMenu> : 
                 <DropDownMenu maxHeight={200} value={state.minute} onChange={(e, i, minute) => this.setState({minute})}>
                   {minutes}
+                </DropDownMenu>
+              </div>
+              <div className='vertical-center'>
+                <span>Timezone Selection: </span>
+                <DropDownMenu maxHeight={200} value={state.timezone} onChange={(e, i, timezone) => this.setState({timezone})}>
+                  {timezones}
                 </DropDownMenu>
               </div>
               <Toggle style={{margin: '10px 0'}} onToggle={this.onToggle} toggled={state.toggled} label='Schedule to send at this time'/>
