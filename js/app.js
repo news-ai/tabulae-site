9376f1f154009c6b9547d3bc6685211547cf3c21
Julie Pan <ycp217@nyu.edu>
2017-04-30 21:07:42 -0400

    hide tag so i can merge other stuff

--- js/app.js
+++ js/app.js
@@ -13,21 +13,21 @@ import 'file?name=[name].[ext]!../manifest.json';
 import 'file?name=[name].[ext]!../.htaccess';
 
 // Check for ServiceWorker support before trying to install it
-if (!window.isDev) {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/serviceworker.js')
-    .then(
-      registration => console.log('ServiceWorker registration successful with scope: ', registration.scope),
-      err => console.log('ServiceWorker registration failed: ', err)
-    ).catch(err => {
-      // Registration failed
-      console.log(err);
-    });
-  } else {
-    // No ServiceWorker Support
-    console.log('service worker is not supported');
-  }
-}
+// if (!window.isDev) {
+//   if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('/serviceworker.js')
+//     .then(
+//       registration => console.log('ServiceWorker registration successful with scope: ', registration.scope),
+//       err => console.log('ServiceWorker registration failed: ', err)
+//     ).catch(err => {
+//       // Registration failed
+//       console.log(err);
+//     });
+//   } else {
+//     // No ServiceWorker Support
+//     console.log('service worker is not supported');
+//   }
+// }
 
 // Import all the third party stuff
 import React from 'react';
