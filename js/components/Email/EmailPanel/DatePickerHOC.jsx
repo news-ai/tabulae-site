bb6bdb186047f3ae403180f03aa74049ecb49b3e
Julie Pan <ycp217@nyu.edu>
2017-06-23 16:03:05 -0400

    add better timezone descriptiongs

--- js/components/Email/EmailPanel/DatePickerHOC.jsx
+++ js/components/Email/EmailPanel/DatePickerHOC.jsx
@@ -6,6 +6,7 @@ import DropDownMenu from 'material-ui/DropDownMenu';
 import MenuItem from 'material-ui/MenuItem';
 import Toggle from 'material-ui/Toggle';
 import FlatButton from 'material-ui/FlatButton';
+import timezones from './timezones';
 
 import moment from 'moment-timezone';
 import 'node_modules/react-datepicker/dist/react-datepicker.css';
@@ -23,45 +24,44 @@ for (let i = 0; i < 60; i++) {
 }
 
 const DATE_FORMAT = 'YYYY-MM-DD HH:mm';
-const UTC_ZONE = 'Europe/London';
 
-const TIMEZONE_NAMES = [
-  {offset: -12.0, text: 'Pacific/Kwajalein'},
-  {offset: -11.0, text: 'Pacific/Samoa'},
-  {offset: -10.0, text: 'Pacific/Honolulu'},
-  {offset: -9.0, text: 'America/Anchorage'},
-  {offset: -8.0, text: 'America/Los_Angeles'},
-  {offset: -7.0, text: 'America/Denver'},
-  {offset: -6.0, text: 'America/Chicago'},
-  {offset: -5.0, text: 'America/New_York'},
-  {offset: -4.0, text: 'Canada/Atlantic'},
-  {offset: -3.5, text: 'Canada/Newfoundland'},
-  {offset: -3.0, text: 'America/Buenos_Aires'},
-  {offset: -2.0, text: 'America/Noronha'},
-  {offset: -1.0, text: 'Atlantic/Azores'},
-  {offset: 0.0, text: 'Europe/London'},
-  {offset: 1.0, text: 'Europe/Brussels'},
-  {offset: 2.0, text: 'Europe/Copenhagen'},
-  {offset: 3.0, text: 'Europe/Moscow'},
-  {offset: 3.5, text: 'Asia/Tehran'},
-  {offset: 4.0, text: 'Asia/Dubai'},
-  {offset: 4.5, text: 'Asia/Kabul'},
-  {offset: 5.0, text: 'Asia/Ashkhabad'},
-  {offset: 5.5, text: 'Asia/Calcutta'},
-  {offset: 5.75, text: 'Asia/Katmandu'},
-  {offset: 6.0, text: 'Asia/Thimbu'},
-  {offset: 6.5, text: 'Asia/Rangoon'},
-  {offset: 7.0, text: 'Asia/Bangkok'},
-  {offset: 8.0, text: 'Asia/Taipei'},
-  {offset: 9.0, text: 'Asia/Tokyo'},
-  {offset: 9.5, text: 'Australia/Darwin'},
-  {offset: 10.0, text: 'Australia/Victoria'},
-  {offset: 11.0, text: 'Asia/Magadan'},
-  {offset: 12.0, text: 'Pacific/Auckland'}
-];
+// const TIMEZONE_NAMES = [
+//   {offset: -12.0, text: 'Pacific/Kwajalein'},
+//   {offset: -11.0, text: 'Pacific/Samoa'},
+//   {offset: -10.0, text: 'Pacific/Honolulu'},
+//   {offset: -9.0, text: 'America/Anchorage'},
+//   {offset: -8.0, text: 'America/Los_Angeles'},
+//   {offset: -7.0, text: 'America/Denver'},
+//   {offset: -6.0, text: 'America/Chicago'},
+//   {offset: -5.0, text: 'America/New_York'},
+//   {offset: -4.0, text: 'Canada/Atlantic'},
+//   {offset: -3.5, text: 'Canada/Newfoundland'},
+//   {offset: -3.0, text: 'America/Buenos_Aires'},
+//   {offset: -2.0, text: 'America/Noronha'},
+//   {offset: -1.0, text: 'Atlantic/Azores'},
+//   {offset: 0.0, text: 'Europe/London'},
+//   {offset: 1.0, text: 'Europe/Brussels'},
+//   {offset: 2.0, text: 'Europe/Copenhagen'},
+//   {offset: 3.0, text: 'Europe/Moscow'},
+//   {offset: 3.5, text: 'Asia/Tehran'},
+//   {offset: 4.0, text: 'Asia/Dubai'},
+//   {offset: 4.5, text: 'Asia/Kabul'},
+//   {offset: 5.0, text: 'Asia/Ashkhabad'},
+//   {offset: 5.5, text: 'Asia/Calcutta'},
+//   {offset: 5.75, text: 'Asia/Katmandu'},
+//   {offset: 6.0, text: 'Asia/Thimbu'},
+//   {offset: 6.5, text: 'Asia/Rangoon'},
+//   {offset: 7.0, text: 'Asia/Bangkok'},
+//   {offset: 8.0, text: 'Asia/Taipei'},
+//   {offset: 9.0, text: 'Asia/Tokyo'},
+//   {offset: 9.5, text: 'Australia/Darwin'},
+//   {offset: 10.0, text: 'Australia/Victoria'},
+//   {offset: 11.0, text: 'Asia/Magadan'},
+//   {offset: 12.0, text: 'Pacific/Auckland'}
+// ];
 
-const timezones = TIMEZONE_NAMES
-.map((timezone, i) => <MenuItem value={timezone.text} key={`timezone-${i}`} primaryText={timezone.text}/>);
+const timezoneMenuItems = Object.keys(timezones)
+.map((timezone, i) => <MenuItem value={timezones[timezone]} key={`timezone-${i}`} primaryText={timezone}/>);
 
 class DatePickerHOC extends Component {
   constructor(props) {
@@ -74,7 +74,7 @@ class DatePickerHOC extends Component {
       hour: rightNow.hours(),
       minute: rightNow.minutes(),
       toggled: false,
-      timezone: TIMEZONE_NAMES[7].text,
+      timezone: moment.tz.guess(),
     };
     this.onToggle = this._onToggle.bind(this);
     this.onRequestOpen = this._onRequestOpen.bind(this);
@@ -131,6 +131,7 @@ class DatePickerHOC extends Component {
     const state = this.state;
     const m = moment(state.date);
 
+
     return (
       <div className={props.className} >
         <Dialog actions={[<FlatButton label='Close' onClick={this.onRequestClose}/>]}
@@ -162,7 +163,7 @@ class DatePickerHOC extends Component {
               <div className='vertical-center'>
                 <span>Timezone Selection: </span>
                 <DropDownMenu maxHeight={200} value={state.timezone} onChange={(e, i, timezone) => this.setState({timezone})}>
-                  {timezones}
+                  {timezoneMenuItems}
                 </DropDownMenu>
               </div>
               <Toggle style={{margin: '10px 0'}} onToggle={this.onToggle} toggled={state.toggled} label='Schedule to send at this time'/>
