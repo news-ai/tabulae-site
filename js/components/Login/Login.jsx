7defd20a16d243d95d656751b4340e0be12c2dff
Julie Pan <ycp217@nyu.edu>
2017-07-24 17:02:29 -0400

    styling login page

--- js/components/Login/Login.jsx
+++ js/components/Login/Login.jsx
@@ -2,32 +2,58 @@ import React from 'react';
 import {connect} from 'react-redux';
 import * as actions from './actions';
 import RaisedButton from 'material-ui/RaisedButton';
-
-const buttonStyle = {
-	textTransform: 'none',
-	margin: '0.1em'
-};
+import FontIcon from 'material-ui/FontIcon';
 
 const styles = {
 	container: {
 		display: 'flex',
 		alignItems: 'center',
 		justifyContent: 'center',
-		minHeight: '50em'
+		height: '100vh',
 	},
+	defaultBtnContainer: {
+		borderBottom: '1px solid lightgray',
+		padding: '0 20px 10px 20px',
+		marginBottom: 10
+	},
+	button: {
+		style: {
+			margin: '0.2em'
+		}
+	},
+	googButton: {
+		labelStyle: {textTransform: 'none', color: '#ffffff'}
+	}
 };
 
-function Login({loginWithGoogleClick, loginClick, registerClick}) {
+const Login = ({loginWithGoogleClick, loginClick, registerClick}) => {
 	return (
 		<div style={styles.container}>
 			<div>
-				<RaisedButton style={buttonStyle} label='Login with Google' onClick={loginWithGoogleClick} />
-				<RaisedButton style={buttonStyle} label='Register' onClick={registerClick} />
-				<RaisedButton style={buttonStyle} label='Login' onClick={loginClick} />
+		{/*
+				<div style={{marginBottom: 50}} >
+					<img src='https://storage.googleapis.com/tabulae-email-images/5689413791121408-IONAfFwuOEc0MJaeNbEGAk2oYYZl1YydCEgkoomRozY=-CopyofCopyofFINAL.png' />
+				</div>
+		*/}
+				<div style={styles.defaultBtnContainer} className='vertical-center horizontal-center'>
+					<div>
+						<RaisedButton style={styles.button.style} label='Register' onClick={registerClick} />
+						<RaisedButton style={styles.button.style} label='Login' onClick={loginClick} />
+					</div>
+				</div>
+				<div className='vertical-center horizontal-center'>
+					<RaisedButton
+					backgroundColor='#4885ed'
+					icon={<FontIcon color='#ffffff' className='fa fa-google' />}
+					labelStyle={styles.googButton.labelStyle}
+					label='Sign-in with Google'
+					onClick={loginWithGoogleClick}
+					/>
+				</div>
 			</div>
 		</div>
 	);
-}
+};
 
 const mapStateToProps = state => {
 	return {
