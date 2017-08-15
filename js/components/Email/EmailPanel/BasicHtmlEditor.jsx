d595e9ce0198c4f10068ed6c3134a75f798af62a
Julie Pan <ycp217@nyu.edu>
2017-08-15 16:24:09 -0400

    convert px to pt to preserve line-height

--- js/components/Email/EmailPanel/BasicHtmlEditor.jsx
+++ js/components/Email/EmailPanel/BasicHtmlEditor.jsx
@@ -580,6 +580,7 @@ class BasicHtmlEditor extends Component {
     let contentState;
 
     if (html) {
+      console.log(html);
       const saneHtml = sanitizeHtml(html, {
         allowedTags: sanitizeHtml.defaults.allowedTags.concat(['span']),
         allowedAttributes: {
@@ -589,6 +590,7 @@ class BasicHtmlEditor extends Component {
           a: ['href']
         }
       });
+      console.log(saneHtml);
       contentState = convertFromHTML(this.CONVERT_CONFIGS)(saneHtml);
       blockMap = contentState.getBlockMap();
     } else {
