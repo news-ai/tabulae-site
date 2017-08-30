95203b63e1c8e0e1339ddfda50fb92ff4366289c
Julie Pan <ycp217@nyu.edu>
2017-08-30 15:05:00 -0400

    show link when hover

--- js/components/Email/EmailPanel/components/Link.js
+++ js/components/Email/EmailPanel/components/Link.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
 
 export default function Link(props) {
   let href;
   if (props.entityKey !== null) {
-    href = props.contentState.getEntity(props.entityKey).getData().href;
+    href = props.contentState.getEntity(props.entityKey).getData().url;
   } else {
     href = props.decoratedText;
   }
   return (
-    <a href={href} target='_blank'>
-      {props.children}
-    </a>
+    <div style={{display: 'inline-block'}} >
+      <a href={href} data-tip data-for='tooltipTarget' target='_blank'>
+        {props.children}
+      </a>
+      <ReactTooltip
+      id='tooltipTarget'
+      place='top'
+      type='dark'
+      effect='solid'
+      getContent={() => href}
+      />
+    </div>
   );
 }
